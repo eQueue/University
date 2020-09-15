7e0f09c73c3b3975f96a276f359d18da982fd640
LeeGyuHong <latancy486@naver.com>
2020-09-16 03:10:17 +0900

    -m .gitignore

--- Project-SRservice/Back-end/Deploy/temp/kr/superresolution/config/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package kr.superresolution.config;
-
-import org.springframework.context.annotation.*;
-
-@Configuration
-@ComponentScan(basePackages = {"kr.superresolution"})
-public class ApplicationConfig {
-
-}
--- Project-SRservice/Back-end/Deploy/temp/kr/superresolution/controller/FileController.java
+++
@@ -1,5 +0,0 @@
-package kr.superresolution.controller;
-
-public class FileController {
-
-}
